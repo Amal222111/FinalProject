26160cac29868dfadfdef846269fa309b253f6e7
user15372282981 <tiktok.uck@gmail.com>
2024-12-27 18:17:09 +0400

    first commit

---
+++ src/main/java/com/example/finalfullstackproject/FinalfullstackprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalfullstackproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalfullstackprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalfullstackprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finalfullstackproject/FinalfullstackprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalfullstackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalfullstackprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
